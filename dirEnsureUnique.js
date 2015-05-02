84d65adf7a1879fe6d1298c69e738c1e7f8d3088
dangerrangerous <dangerousdevops@gmail.com>
2015-05-01 17:00:36 -0800

    validations visible in jsFid, not in jsBin...

--- dirEnsureUnique.js
+++ dirEnsureUnique.js
@@ -1,20 +1,19 @@
-app.directive('ensureUnique', function($http) {
-	return {
-		require: 'ngModel',
-		link: function(scope, ele, attrs, c) {
-			scope.$watch(attrs.ngModel, function(n) {
-				if(!n) return;
-				$http({
-					method: 'POST',
-					// backend server routing
-					url: '/api/check/'+attrs.ensureUnique,
-					data: {'field': attrs.ensureUnique}
-				}).success(function(data) {
-					c.$setValidity('unique', data.isUnique);
-				}).error(function(data) {
-					c.$setValidity('unique', false);
-				});
-			});
-		}
-	}
-});
+angular.module('myApp', [])
+.directive('ensureUnique', ['$http', function($http) {
+  return {
+    require: 'ngModel',
+    link: function(scope, ele, attrs, c) {
+      scope.$watch(attrs.ngModel, function() {
+        $http({
+          method: 'POST',
+          url: '/api/check/' + attrs.ensureUnique,
+          data: {'field': attrs.ensureUnique}
+        }).success(function(data, status, headers, cfg) {
+          c.$setValidity('unique', data.isUnique);
+        }).error(function(data, status, headers, cfg) {
+          c.$setValidity('unique', false);
+        });
+      });
+    }
+  };
+}]);
